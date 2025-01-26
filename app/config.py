6245982e5c6f5c7203ed153acc055b9d8230e099
patrick McWilliams <pmcwilliams@tandemdiabetes.com>
2025-01-26 05:05:14 -0800

    no message

---
+++ app/config.py
@@ -0,0 +1,22 @@
+import os
+
+# telegram bot credentials
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ALLOWED_GROUP_ID = int(os.getenv('ALLOWED_GROUP_ID', 0))
+ADMIN_USER_ID = int(os.getenv('ADMIN_USER_ID', 0))
+WEBHOOK_PATH = os.getenv('WEBHOOK_PATH', '/webhook')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+RUN_MODE = os.getenv('RUN_MODE', 'webhook')
+
+# poe API credentials
+POE_API_KEY = os.getenv('POE_API_KEY')
+
+# huggingface API credentials
+HF_TOKEN = os.getenv('HF_TOKEN')
+
+# Play.ht API credentials
+PLAY_HT_USER_ID = os.getenv('PLAY_HT_USER_ID')
+PLAY_HT_API_KEY = os.getenv('PLAY_HT_API_KEY')
+
+# anthropic API credentials
+ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
